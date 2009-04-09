45917a04b3ea7e75ab6078fc129937d2713d1995
Samuel Ytterbrink <samuelytterbrink@pub97-89.pub.luth.se>
2009-04-09 19:47:39 +0200

    comited last changes and removed nnc from git repo

--- src/script.js
+++ src/script.js
@@ -7,10 +7,11 @@ function searchLocalDir(e){
 	ajax.onreadystatechange=function(){
 		document.getElementById("loadingBar").style.width=(100/4)*ajax.readyState+"%";
 		if(ajax.readyState==4){
-			ncc=crunchNCC(ajax.responseText);
+			rep=ajax.responseText;
+			ncc=crunchNCC(rep);
 			document.getElementById("toc").innerHTML=ncc;
-			smiles=extraxtSmilUrls(ncc);
-			fetchSimels(smiles);
+			smiles=extraxtSmilUrls(rep);
+			fetchSmils(smiles);
 			document.getElementById("loadingMessage").innerText="Sucessfully loaded Ncc";
 		}
 	};
@@ -18,27 +19,78 @@ function searchLocalDir(e){
 	ajax.send(null);
 
 }
-function fetchSimels(){
-	return;
+function fetchSmils(smiles){
+	max=smiles.length
+	document.smiles=smiles;
+	recived=0;
+	bar=document.getElementById("loadingBar");
+	message=document.getElementById("loadingMessage");
+	smilData=document.getElementById("smilData");
+	smilData.innerHTML="";
+	bar.style.width="0%";
+	message.innerText="Start Loading Smil Files..";
+	buffer={};
+	if(smiles==null || smiles==[]){
+		alert("error");
+		return;
+	}
+	for(i in smiles){
+		ajax=getAjax();
+		name=smiles[i]
+		function onreadystatechange(){
+			if(ajax.readyState==4){
+				buffer[name]=ajax.responseText;
+				recived+=1;
+				bar.style.width=(100/max)*recived+"%";
+				if(recived=max){
+					document.buffer=buffer;	
+					message.innerText="Recived all Smil Files crunching data...";
+					for(x in document.buffer){
+						document.buffer[x]=crunchSMIL(document.buffer[x]);
+					}
+					message.innerText="Done crunching you can now Play";
+					document.number=recived;
+
+				}
+				else{
+					message.innerText="Recived "+recived+" of "+max+" Smil Files";
+				}
+			}
+		}
+		ajax.onreadystatechange=onreadystatechange;
+		ajax.open("GET",smiles[i],false);
+		ajax.send(null);
+		//bar.style.width=100/max*(i+1)+"%";
+	}
 	}
 function crunchNCC(ncc){
 	ncc=ncc.toLowerCase();
 	ncc=ncc.match(/<body>((:?.|\n)*)<\/body>/)[0];
-	ncc=ncc.replace(/<a(.*)href=".*#(.*)"(.*)>(.*)<\/a>/g,"<div $1onClick=\"play(\'$2\');\"$3>$4</div>")
+	ncc=ncc.replace(/<a(.*)href="(.*)#(.*)"(.*)>(.*)<\/a>/g,"<div $1onClick=\"play(\'$2\',\'$3\');\"$4>$5</div>")
 	return ncc;
 }
-function play(id){
-	document.getElementById('loadingMessage').innerText="Playing id "+id;
+function play(smilFile,id){
+	document.getElementById("smilData").innerHTML=document.buffer[smilFile];	
+	document.current=document.getElementById(id).getElementsByTagName("audio")[0]
+	document.current.onhashchange=function (e){ alert(this.paused);}
+	document.current.play()
+	document.getElementById('loadingMessage').innerText="Playing id "+id+" in file "+smilFile;
+}
+function stop(){
+	document.current.pause();
 }
 function crunchSMIL(smil){
 	smil=smil.toLowerCase();
 	smil=smil.replace(/clip-begin=/g,"start=");
 	smil=smil.replace(/clip-end=/g,"end=");
-	smil=smil.replace(/npt=(\d+\.\d*)s/g,'$1')
+	smil=smil.replace(/npt=(\d+\.\d)\d*s*/g,'$1')
 	return smil;
 }
 
 function extraxtSmilUrls(ncc){
-	return [];
+	document.ncc=ncc;
+	ret=ncc.match(/\w*.smil/g);
+	ret=ret.filter(function (a,i,t){return t.indexOf(a)==i;});
+	return ret!=null?ret:[];
 }
 addEventListener("load",searchLocalDir,false);
